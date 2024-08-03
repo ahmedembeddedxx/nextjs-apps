6f59c3848f152e7033bb1b358ee079587e836e1e
Ahmad Abdullah <ahmedembedded@gmail.com>
2024-08-03 18:15:39 +0500

    Update firebase.js

--- pantry-tracker/lib/firebase.js
+++ pantry-tracker/lib/firebase.js
@@ -3,13 +3,13 @@ import { getDatabase, ref, set, get, update, remove, child } from "firebase/data
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCX92c5vRgFC4XVCTYPgvzMB1uhnpJumGc",
-  authDomain: "pantry-tracker-4d365.firebaseapp.com",
-  projectId: "pantry-tracker-4d365",
-  storageBucket: "pantry-tracker-4d365.appspot.com",
-  messagingSenderId: "678727779265",
-  appId: "1:678727779265:web:1ab1b28bf4df4b5e4ea540",
-  measurementId: "G-46LYJJW0HC"
+  apiKey: "**************",
+  authDomain: "**************",
+  projectId: "**************",
+  storageBucket: "**************",
+  messagingSenderId: "**************",
+  appId: "**************",
+  measurementId: "**************"
 };
 
 const app = initializeApp(firebaseConfig);
